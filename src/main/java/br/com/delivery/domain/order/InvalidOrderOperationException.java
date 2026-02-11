f8d15fed39aa180347d6bc25f9b7bd35c56717e8
Smeltier <connect.goncalves@gmail.com>
2026-02-11 18:44:13 -0300

    att: atualiza pacote de InvalidOrderOperationException.

--- src/main/java/br/com/delivery/domain/order/InvalidOrderOperationException.java
+++ src/main/java/br/com/delivery/domain/order/InvalidOrderOperationException.java
@@ -1,3 +1,5 @@
+package br.com.delivery.domain.order;
+
 public class InvalidOrderOperationException extends RuntimeException {
   public InvalidOrderOperationException(String message) {
     super(message);
