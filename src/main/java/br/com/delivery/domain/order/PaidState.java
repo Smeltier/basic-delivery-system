3120dffe89c641e9b0d75a78f28f98948ddf285f
Smeltier <connect.goncalves@gmail.com>
2026-02-11 18:47:27 -0300

    att: atualiza o pacote da classe PaidState.

--- src/main/java/br/com/delivery/domain/order/PaidState.java
+++ src/main/java/br/com/delivery/domain/order/PaidState.java
@@ -1,24 +1,21 @@
-package domain.order;
+package br.com.delivery.domain.order;
 
 public class PaidState implements OrderState {
   @Override
   public void addItem(Order order, OrderItem item) {
     throw new InvalidOrderOperationException(
-        "Não se pode adicionar novos itens a um pedido já pago."
-    );
+        "Não se pode adicionar novos itens a um pedido já pago.");
   }
 
   @Override
   public void pay(Order order) {
     throw new InvalidOrderOperationException(
-        "Não se pode pagar novamente um pedido que já foi pago."
-    );
+        "Não se pode pagar novamente um pedido que já foi pago.");
   }
 
   @Override
   public void cancel(Order order) {
     throw new InvalidOrderOperationException(
-        "Não se pode cancelar um pedido que já foi pago."
-    );
+        "Não se pode cancelar um pedido que já foi pago.");
   }
 }
