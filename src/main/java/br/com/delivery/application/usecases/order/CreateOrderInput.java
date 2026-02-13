1a9f0bed5be5412ff225b5c8d03602ad2f58107f
Gabriel G. <connect.goncalves@gmail.com>
2026-02-13 18:59:32 -0300

    add: cria o record CreateOrderInput.java

--- src/main/java/br/com/delivery/application/usecases/order/CreateOrderInput.java
+++ src/main/java/br/com/delivery/application/usecases/order/CreateOrderInput.java
@@ -1,6 +1,14 @@
 package br.com.delivery.application.usecases.order;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
-public record CreateOrderInput(UUID id, List<ItemInput> items) {
+import br.com.delivery.domain.shared.Currency;
+
+public record CreateOrderInput(UUID clientId, Currency currency, List<ItemInput> items) {
+  public CreateOrderInput {
+    clientId = Objects.requireNonNull(clientId);
+    currency = Objects.requireNonNull(currency);
+  }
 }
