0a2e0aed78b57d21ecc61683b2e3d3991c9be75c
Gabriel G. <connect.goncalves@gmail.com>
2026-02-12 23:21:10 -0300

    add: cria a classe Client no domínio.

--- src/main/java/br/com/delivery/domain/client/Client.java
+++ src/main/java/br/com/delivery/domain/client/Client.java
@@ -1,5 +1,59 @@
-package br.com.delivery.domain;
+package br.com.delivery.domain.client;
+
+import br.com.delivery.domain.shared.Email;
 
 public class Client {
+  private final ClientId id;
+  private Email email;
+  private String name;
+  private boolean active;
 
+  public Client(final ClientId id, final String name, final Email email) {
+    if (id == null) {
+      throw new IllegalArgumentException("ID não pode ser nulo.");
+    }
+    this.id = id;
+    this.active = true;
+
+    setEmail(email);
+    setName(name);
+  }
+
+  public boolean isActive() {
+    return this.active;
+  }
+
+  public void deactivate() {
+    this.active = false;
+  }
+
+  public void activate() {
+    this.active = true;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ClientId getId() {
+    return id;
+  }
+
+  public Email getEmail() {
+    return email;
+  }
+
+  public void setEmail(Email newEmail) {
+    if (newEmail == null) {
+      throw new IllegalArgumentException("Email inválido.");
+    }
+    this.email = newEmail;
+  }
+
+  public void setName(String newName) {
+    if (newName == null || newName.isBlank()) {
+      throw new IllegalArgumentException("Nome inválido");
+    }
+    this.name = newName;
+  }
 }
