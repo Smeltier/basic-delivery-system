60400cb1cbabd838c954818d92ee0f44936d82a3
Gabriel G. <connect.goncalves@gmail.com>
2026-02-13 19:02:21 -0300

    att: ?

--- src/main/java/br/com/delivery/domain/order/OrderItem.java
+++ src/main/java/br/com/delivery/domain/order/OrderItem.java
@@ -1,6 +1,5 @@
 package br.com.delivery.domain.order;
 
-import br.com.delivery.domain.product.Product;
 import br.com.delivery.domain.product.ProductId;
 import br.com.delivery.domain.shared.Money;
 
