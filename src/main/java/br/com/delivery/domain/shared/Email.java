2d723fdd150e8a330f11bf0d4f821617d2501e66
Gabriel G. <connect.goncalves@gmail.com>
2026-02-13 22:54:42 -0300

    fix: corrige erro no pattern da classe Email.

--- src/main/java/br/com/delivery/domain/shared/Email.java
+++ src/main/java/br/com/delivery/domain/shared/Email.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 
 public record Email(String value) {
-  private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]@[A-Za-z0-9.-]+$");
+  private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
 
   public Email {
     Objects.requireNonNull(value);
