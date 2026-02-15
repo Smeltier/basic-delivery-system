c169ce96914f64eb9a44bdd966a49ab9463a9c1f
Smeltier <connect.goncalves@gmail.com>
2026-02-15 18:29:41 -0300

    att: adiciona OrderStatus e PaymentStatus como enums, removendo o State Pattern.

---
+++ src/main/java/br/com/delivery/domain/order/OrderStatus.java
@@ -0,0 +1,8 @@
+package br.com.delivery.domain.order;
+
+public enum OrderStatus {
+  CREATED,
+  PAID,
+  CONFIRMED,
+  CANCELLED,
+}
---
+++ src/main/java/br/com/delivery/domain/payment/PaymentStatus.java
@@ -0,0 +1,9 @@
+package br.com.delivery.domain.payment;
+
+public enum PaymentStatus {
+  PENDING,
+  APPROVED,
+  DECLINED,
+  CANCELLED,
+  REFUNDED
+}
