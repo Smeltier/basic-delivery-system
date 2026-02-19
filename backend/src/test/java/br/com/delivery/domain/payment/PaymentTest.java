39767ca2dfc74956b11ebdc76b921000c4080b6e
Smeltier <connect.goncalves@gmail.com>
2026-02-19 13:09:39 -0300

    add: adiciona testes para a classe Payment.

--- backend/src/test/java/br/com/delivery/domain/payment/PaymentTest.java
+++ backend/src/test/java/br/com/delivery/domain/payment/PaymentTest.java
@@ -7,16 +7,76 @@ import static org.junit.jupiter.api.Assertions.*;
 import br.com.delivery.domain.order.OrderId;
 import br.com.delivery.domain.shared.Currency;
 import br.com.delivery.domain.shared.Money;
+import br.com.delivery.domain.exception.InvalidPaymentOperationException;
 
 public class PaymentTest {
   private final Money amount = new Money(BigDecimal.TEN, Currency.BRL);
   private final FakeApprovedPaymentMethod fakeApprovedMethod = new FakeApprovedPaymentMethod();
-  // private final FakeRejectedPaymentMethod fakeRejectedMethod = new
-  // FakeRejectedPaymentMethod();
+  private final FakeRejectedPaymentMethod fakeRejectedMethod = new FakeRejectedPaymentMethod();
 
   @Test
   void shouldCreateWithPendingStatus() {
     Payment payment = Payment.create(OrderId.generate(), this.fakeApprovedMethod, amount);
     assertEquals(PaymentStatus.PENDING, payment.getStatus());
+  }
+
+  @Test
+  void shouldThrowWhenCancelAProcessedPayment() {
+    Payment payment = Payment.create(OrderId.generate(), this.fakeApprovedMethod, this.amount);
+    payment.process();
+
+    assertThrows(InvalidPaymentOperationException.class,
+        () -> payment.cancel());
+  }
+
+  @Test
+  void shouldThrowWhenProcessAProcessedPayment() {
+    Payment payment = Payment.create(OrderId.generate(), this.fakeApprovedMethod, this.amount);
+    payment.process();
+
+    assertThrows(InvalidPaymentOperationException.class,
+        () -> payment.process());
+  }
+
+  @Test
+  void shouldThrowWhenRefundAUnapprovedPayment() {
+    Payment payment = Payment.create(OrderId.generate(), this.fakeRejectedMethod, this.amount);
+    payment.process();
+
+    assertThrows(InvalidPaymentOperationException.class,
+        () -> payment.refund());
+  }
+
+  @Test
+  void statusShouldBeApprovedWhenPaymentIsApproved() {
+    Payment payment = Payment.create(OrderId.generate(), this.fakeApprovedMethod, this.amount);
+    payment.process();
+
+    assertEquals(PaymentStatus.APPROVED, payment.getStatus());
+  }
+
+  @Test
+  void statusShouldBeDeclinedWhenPaymentIsRejected() {
+    Payment payment = Payment.create(OrderId.generate(), this.fakeRejectedMethod, this.amount);
+    payment.process();
+
+    assertEquals(PaymentStatus.DECLINED, payment.getStatus());
+  }
+
+  @Test
+  void shouldCancelPendingPayment() {
+    Payment payment = Payment.create(OrderId.generate(), this.fakeRejectedMethod, this.amount);
+    payment.cancel();
+
+    assertEquals(PaymentStatus.CANCELLED, payment.getStatus());
+  }
+
+  @Test
+  void shouldRefundApprovedPayment() {
+    Payment payment = Payment.create(OrderId.generate(), this.fakeApprovedMethod, this.amount);
+    payment.process();
+    payment.refund();
+
+    assertEquals(PaymentStatus.REFUNDED, payment.getStatus());
   }
 }
