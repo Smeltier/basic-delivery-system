330c5eaea5dc1d0238a41a5d50742bca73535988
Smeltier <connect.goncalves@gmail.com>
2026-02-20 18:50:37 -0300

    att: atualiza implementação do record Money.

--- backend/src/main/java/br/com/delivery/domain/shared/Money.java
+++ backend/src/main/java/br/com/delivery/domain/shared/Money.java
@@ -9,11 +9,19 @@ import br.com.delivery.domain.exception.InsufficientFundsException;
 
 public record Money(BigDecimal amount, Currency currency) {
   public Money {
-    if (isNegative(amount)) {
-      throw new InvalidMoneyOperationException("A quantiade não pode ser negativa.");
+    Objects.requireNonNull(amount);
+    Objects.requireNonNull(currency);
+    if (amount.compareTo(BigDecimal.ZERO) < 0) {
+      throw new InvalidMoneyOperationException("A quantidade não pode ser negativa.");
     }
-    amount = Objects.requireNonNull(amount);
-    currency = Objects.requireNonNull(currency);
+  }
+
+  public static Money of(double amount, Currency currency) {
+    return new Money(BigDecimal.valueOf(amount), currency);
+  }
+
+  public static Money of(BigDecimal amount, Currency currency) {
+    return new Money(amount, currency);
   }
 
   public static Money zero(Currency currency) {
@@ -21,23 +29,23 @@ public record Money(BigDecimal amount, Currency currency) {
   }
 
   public Money add(Money value) {
-    this.ensureSameCurrency(value);
+    ensureSameCurrency(value);
     return new Money(this.amount.add(value.amount), this.currency);
   }
 
   public Money subtract(Money value) {
-    if (!isGreaterThanOrEqual(value)) {
-      throw new InsufficientFundsException("O valor passado deve ser maior ou igual ao valor atual.");
+    ensureSameCurrency(value);
+    if (this.amount.compareTo(value.amount) < 0) {
+      throw new InsufficientFundsException("Saldo insuficiente.");
     }
-    this.ensureSameCurrency(value);
     return new Money(this.amount.subtract(value.amount), this.currency);
   }
 
-  public Money multiply(int value) {
-    if (value < 0) {
-      throw new InvalidMoneyOperationException("O valor não pode ser zero.");
+  public Money multiply(int factor) {
+    if (factor < 0) {
+      throw new InvalidMoneyOperationException("O fator não pode ser negativo.");
     }
-    return new Money(this.amount.multiply(BigDecimal.valueOf(value)), this.currency);
+    return new Money(this.amount.multiply(BigDecimal.valueOf(factor)), this.currency);
   }
 
   public boolean isPositive() {
@@ -48,17 +56,9 @@ public record Money(BigDecimal amount, Currency currency) {
     return amount.compareTo(BigDecimal.ZERO) == 0;
   }
 
-  public boolean isGreaterThanOrEqual(Money value) {
-    return this.amount.compareTo(value.amount) >= 0;
-  }
-
-  private boolean isNegative(BigDecimal amount) {
-    return amount.compareTo(BigDecimal.ZERO) < 0;
-  }
-
   private void ensureSameCurrency(Money value) {
     if (!this.currency.equals(value.currency)) {
-      throw new CurrencyMismatchException("Operações não podem ser feitas entre moedas diferentes.");
+      throw new CurrencyMismatchException("Moedas diferentes.");
     }
   }
 }
