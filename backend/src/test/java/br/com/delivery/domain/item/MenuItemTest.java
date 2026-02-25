db91219f65a5ac88aae1aeadc0511d440d1a4e5e
Smeltier <connect.goncalves@gmail.com>
2026-02-25 13:02:17 -0300

    add: adiciona testes para MenuItem.

---
+++ backend/src/test/java/br/com/delivery/domain/item/MenuItemTest.java
@@ -0,0 +1,38 @@
+package br.com.delivery.domain.item;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import br.com.delivery.domain.exception.InactiveItemException;
+import br.com.delivery.domain.restaurant.RestaurantId;
+import br.com.delivery.domain.shared.Currency;
+import br.com.delivery.domain.shared.Money;
+
+public class MenuItemTest {
+  @Test
+  void shouldThrowWhenChangePriceInAInactiveProduct() {
+    Money price = Money.of(5, Currency.BRL);
+    MenuItemId id = MenuItemId.generate();
+    RestaurantId restaurantId = RestaurantId.generate();
+    MenuItemCategory category = MenuItemCategory.DESSERT;
+    MenuItem item = new MenuItem(id, restaurantId, "item", "description", category, price);
+    item.deactivate();
+
+    assertThrows(InactiveItemException.class,
+        () -> item.changePrice(Money.of(2, Currency.BRL)));
+  }
+
+  @Test
+  void shouldCreateValidMenuItem() {
+    Money price = Money.of(5, Currency.BRL);
+    MenuItemId id = MenuItemId.generate();
+    RestaurantId restaurantId = RestaurantId.generate();
+    MenuItemCategory category = MenuItemCategory.DESSERT;
+    MenuItem item = new MenuItem(id, restaurantId, "item", "description", category, price);
+
+    assertEquals(price, item.currentPrice());
+    assertEquals(id, item.getId());
+    assertEquals(restaurantId, item.getRestaurantId());
+    assertEquals(category, item.getCategory());
+  }
+}
