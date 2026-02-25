d35b6cc5ecf5db4939f49203347d72dbf4ab0263
Smeltier <connect.goncalves@gmail.com>
2026-02-25 12:45:34 -0300

    att: atualiza testes de OrderItem.

--- backend/src/test/java/br/com/delivery/domain/order/OrderItemTest.java
+++ backend/src/test/java/br/com/delivery/domain/order/OrderItemTest.java
@@ -5,46 +5,53 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import br.com.delivery.domain.product.ProductId;
+import br.com.delivery.domain.exception.InvalidOrderItemException;
+import br.com.delivery.domain.exception.InvalidOrderItemQuantityException;
+import br.com.delivery.domain.item.MenuItemCategory;
+import br.com.delivery.domain.item.MenuItemId;
 import br.com.delivery.domain.shared.Currency;
 import br.com.delivery.domain.shared.Money;
 
 public class OrderItemTest {
-  private final ProductId productId = ProductId.generate();
-  private final String productName = "produto";
+  private final MenuItemId menuItemId = MenuItemId.generate();
+  private final String menuItemName = "item";
+  private final String menuItemDescription = "description";
   private final Money unitPrice = Money.zero(Currency.BRL);
 
   @Test
   void shouldThrowWhenQuantityIsNegative() {
-    assertThrows(IllegalArgumentException.class,
-        () -> new OrderItem(this.productId, this.productName, this.unitPrice, -10));
+    assertThrows(InvalidOrderItemQuantityException.class,
+        () -> new OrderItem(menuItemId, menuItemName, menuItemDescription, MenuItemCategory.DESSERT, unitPrice, -10));
   }
 
   @Test
   void shouldThrowWhenQuantityIsZero() {
-    assertThrows(IllegalArgumentException.class,
-        () -> new OrderItem(this.productId, this.productName, this.unitPrice, 0));
+    assertThrows(InvalidOrderItemQuantityException.class,
+        () -> new OrderItem(menuItemId, menuItemName, menuItemDescription, MenuItemCategory.DESSERT, unitPrice, 0));
   }
 
   @Test
   void shouldThrowWhenProductNameIsBlank() {
-    assertThrows(IllegalArgumentException.class,
-        () -> new OrderItem(this.productId, "", this.unitPrice, 2));
+    assertThrows(InvalidOrderItemException.class,
+        () -> new OrderItem(menuItemId, "", menuItemDescription, MenuItemCategory.DESSERT, unitPrice, 2));
   }
 
   @Test
   void shouldCreateOrderItemWithCorrectValue() {
-    OrderItem item = new OrderItem(this.productId, this.productName, this.unitPrice, 5);
-    assertEquals(this.productId, item.getProductId());
-    assertEquals(this.productName, item.getProductName());
+    OrderItem item = new OrderItem(menuItemId, menuItemName, menuItemDescription, MenuItemCategory.DESSERT, unitPrice,
+        2);
+    assertEquals(this.menuItemId, item.getMenuItemId());
+    assertEquals(this.menuItemName, item.getMenuItemName());
+    assertEquals(this.menuItemDescription, item.getMenuItemDescription());
+    assertEquals(MenuItemCategory.DESSERT, item.getMenuItemCategory());
     assertEquals(this.unitPrice, item.getUnitPrice());
-    assertEquals(5, item.getQuantity());
+    assertEquals(2, item.getQuantity());
   }
 
   @Test
   void shouldCalculateTotalCorrectly() {
     Money price = new Money(new BigDecimal("10.50"), Currency.BRL);
-    OrderItem item = new OrderItem(productId, productName, price, 2);
+    OrderItem item = new OrderItem(menuItemId, menuItemName, menuItemDescription, MenuItemCategory.DESSERT, price, 2);
 
     Money total = new Money(new BigDecimal("21.00"), Currency.BRL);
 
