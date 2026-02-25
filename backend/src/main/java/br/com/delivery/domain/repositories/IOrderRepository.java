cb2b0ea98d60292817f9a48d205413e825379cd7
Smeltier <connect.goncalves@gmail.com>
2026-02-25 17:26:09 -0300

    att: adiciona método para encontrar Orders que estejam em DRAFT via AccountId e RestaurantId.

--- backend/src/main/java/br/com/delivery/domain/repositories/IOrderRepository.java
+++ backend/src/main/java/br/com/delivery/domain/repositories/IOrderRepository.java
@@ -3,10 +3,14 @@ package br.com.delivery.domain.repositories;
 import java.util.Optional;
 
 import br.com.delivery.domain.order.OrderId;
+import br.com.delivery.domain.restaurant.RestaurantId;
+import br.com.delivery.domain.account.AccountId;
 import br.com.delivery.domain.order.Order;
 
 public interface IOrderRepository {
   Optional<Order> findById(OrderId id);
+
+  Optional<Order> findDraftByClientAndRestaurant(AccountId accountId, RestaurantId restaurantId);
 
   void save(Order order);
 }
