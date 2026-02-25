ea253a2979a98239b4786b7544387ba024360e34
Smeltier <connect.goncalves@gmail.com>
2026-02-25 11:12:02 -0300

    att: .

--- backend/src/main/java/br/com/delivery/application/usecases/order/CreateOrderInput.java
+++ backend/src/main/java/br/com/delivery/application/usecases/order/CreateOrderInput.java
@@ -1,14 +1,15 @@
-package br.com.delivery.application.usecases.order;
-
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
-
-import br.com.delivery.domain.shared.Currency;
-
-public record CreateOrderInput(UUID clientId, Currency currency, List<ItemInput> items) {
-  public CreateOrderInput {
-    clientId = Objects.requireNonNull(clientId);
-    currency = Objects.requireNonNull(currency);
-  }
-}
+// package br.com.delivery.application.usecases.order;
+//
+// import java.util.List;
+// import java.util.Objects;
+// import java.util.UUID;
+//
+// import br.com.delivery.domain.shared.Currency;
+//
+// public record CreateOrderInput(UUID clientId, Currency currency,
+// List<ItemInput> items) {
+// public CreateOrderInput {
+// clientId = Objects.requireNonNull(clientId);
+// currency = Objects.requireNonNull(currency);
+// }
+// }
--- backend/src/main/java/br/com/delivery/application/usecases/order/ItemInput.java
+++ backend/src/main/java/br/com/delivery/application/usecases/order/ItemInput.java
@@ -1,14 +1,14 @@
-package br.com.delivery.application.usecases.order;
-
-import br.com.delivery.domain.product.ProductId;
-
-import java.util.Objects;
-
-public record ItemInput(ProductId productId, int quantity) {
-  public ItemInput {
-    if (quantity < 0) {
-      throw new IllegalArgumentException("Quantidade não pode ser negativa.");
-    }
-    Objects.requireNonNull(productId);
-  }
-}
+// package br.com.delivery.application.usecases.order;
+//
+// import br.com.delivery.domain.product.ProductId;
+//
+// import java.util.Objects;
+//
+// public record ItemInput(ProductId productId, int quantity) {
+// public ItemInput {
+// if (quantity < 0) {
+// throw new IllegalArgumentException("Quantidade não pode ser negativa.");
+// }
+// Objects.requireNonNull(productId);
+// }
+// }
