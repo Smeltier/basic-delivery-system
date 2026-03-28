a4ca1632b9982e801db78e6001cc7fd396e9568e
Smeltier <connect.goncalves@gmail.com>
2026-03-28 17:51:28 -0300

    att: muda o nome da DTO de RemoveItemFromOrderOutput para DecreseItemQuantityFromOrderOutput.

--- backend/src/main/java/br/com/delivery/application/dto/order/DecreaseItemQuantityFromOrderOutput.java
+++ backend/src/main/java/br/com/delivery/application/dto/order/DecreaseItemQuantityFromOrderOutput.java
@@ -6,8 +6,8 @@ import java.util.Objects;
 import br.com.delivery.domain.order.OrderId;
 import br.com.delivery.domain.shared.Money;
 
-public record RemoveItemFromOrderOutput(OrderId orderId, Money newTotal, List<OrderItemOutput> remainingItems) {
-  public RemoveItemFromOrderOutput {
+public record DecreaseItemQuantityFromOrderOutput(OrderId orderId, Money newTotal, List<OrderItemOutput> remainingItems) {
+  public DecreaseItemQuantityFromOrderOutput {
     orderId = Objects.requireNonNull(orderId, "ID do pedido não pode ser nulo.");
     newTotal = Objects.requireNonNull(newTotal, "Total do pedido não pode ser nulo.");
     remainingItems = Objects.requireNonNull(remainingItems, "Lista de itens restantes não pode ser nula.");
